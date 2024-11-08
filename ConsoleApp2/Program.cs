308708aefb1d5dc5333f46d6be1bdc24f0cfe404
Jackemp <jackpaalomino@gmail.com>
2024-11-08 14:47:07 -0500

    PRIMER COMMIT

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EJERCICIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el número de elementos en el arreglo: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arreglo = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Ingrese el valor en la posición {i + 1}: ");
+                arreglo[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\nArreglo original:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arreglo[i] + " ");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
